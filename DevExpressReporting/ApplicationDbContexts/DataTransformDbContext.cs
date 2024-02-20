0f981631abb30cb688be73899479c9ce82a8a9ce
MBoota <m43577535@gmail.com>
2024-02-20 23:56:06 +0500

    push push

---
+++ DevExpressReporting/ApplicationDbContexts/DataTransformDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoronaReportingTool.ApplicationDbContexts
+{
+    public partial class DataTransformDbContext : DbContext
+	{
+		public DataTransformDbContext(DbContextOptions<DataTransformDbContext> options)
+			: base(options)
+		{
+		}
+		
+    }
+}
